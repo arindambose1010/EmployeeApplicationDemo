4e482c3a7c1c440bcc87f11a3d334929e5adef45
arindambose1010 <39044906+arindambose1010@users.noreply.github.com>
2022-06-15 22:53:11 +0530

    method added

--- src/main/java/com/employee/configuration/LoggerConfig.java
+++ src/main/java/com/employee/configuration/LoggerConfig.java
@@ -4,12 +4,20 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import comm.grpc.logger.service.LogGenerater;
+import comm.grpc.logger.service.LogGeneraterImpl;
+import comm.grpc.logger.service.LogMessageGenerater;
+import comm.grpc.logger.service.LogMessageGeneraterImpl;
 
 @Configuration
 public class LoggerConfig {
 
 	@Bean
 	public LogGenerater logGenerater() {
-	    return new LogGenerater();
+	    return LogGeneraterImpl.builder().build();
+	}
+	
+	@Bean
+	public LogMessageGenerater logMessageGenerater() {
+	    return LogMessageGeneraterImpl.builder().build();
 	}
 }
